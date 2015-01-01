e62c3e88e3223f19c32fc36745310b82e04d93ff
Sushant Nayak <sushantnayak@Sushants-MBP.attlocal.net>
2014-12-31 20:53:56 -0500

    Initial Commit

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Sushant Nayak on 12/31/14.
+//  Copyright (c) 2014 Lambda One Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by Sushant Nayak on 12/31/14.
+//  Copyright (c) 2014 Lambda One Limited. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
